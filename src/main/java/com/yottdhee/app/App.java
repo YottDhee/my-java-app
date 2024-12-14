1779d21b8fc34aa38f4d10a20de05396175fa8c6
YottDhee <jegadevan0079@gmail.com>
2024-12-14 01:44:03 +0000

    Redmi-1397 - Created a Maven Project

---
+++ src/main/java/com/yottdhee/app/App.java
@@ -0,0 +1,13 @@
+package com.yottdhee.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Good Morning!!" );
+    }
+}
