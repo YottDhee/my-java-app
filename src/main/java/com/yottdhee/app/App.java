b8999b5e88dfa8866528a5a5c170bf456084ec10
Jegadevan Vennalkrishnan <134512060+YottDhee@users.noreply.github.com>
2024-12-19 19:42:30 +0530

    Update App.java

--- src/main/java/com/yottdhee/app/App.java
+++ src/main/java/com/yottdhee/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Guys! Good Evening All!!" );
+        System.out.println( "Hello Guys! Good Evening Everyone!!" );
     }
 }
